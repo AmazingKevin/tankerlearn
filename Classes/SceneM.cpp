4f6cb627675564bd71aa898d3a926aa057a7740f
wujinlong <11@11.com>
2018-12-26 15:16:16 +0800

    ziyuan

---
+++ Classes/SceneM.cpp
@@ -0,0 +1,5 @@
+//
+// Created by admin on 2018/12/26.
+//
+
+#include "SceneM.h"
---
+++ Classes/SceneM.h
@@ -0,0 +1,8 @@
+//
+// Created by admin on 2018/12/26.
+//
+
+#ifndef PROJ_ANDROID_SCENEM_H
+#define PROJ_ANDROID_SCENEM_H
+
+#endif //PROJ_ANDROID_SCENEM_H
