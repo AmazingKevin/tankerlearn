f5106b592575223f3f772f4c060e8d0a7e5d63d8
wujinlong <11@11.com>
2018-12-26 15:31:13 +0800

    s

--- Classes/SceneM.h
+++ Classes/SceneM.h
@@ -4,5 +4,40 @@
 
 #ifndef PROJ_ANDROID_SCENEM_H
 #define PROJ_ANDROID_SCENEM_H
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+enum SceneType			// 场景类型
+{
+    kLoadingScene,		// 加载场景
+    kMenuScene,			// 菜单场景
+    kLevelScene,		// 关卡过渡场景
+    kGameScene,			// 游戏场景
+    kGameOverScene		// 游戏结束场景
+};
+
+enum TransitionType		// 场景切换效果类型
+{
+    kTransitionNone,	// 无
+    kTransitionExA,		// 扩展效果A（进入关卡过渡场景的效果）
+    kTransitionExB,		// 扩展效果B（离开关卡过渡场景的效果）
+};
+
+class SceneM:public Ref{
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif //PROJ_ANDROID_SCENEM_H
