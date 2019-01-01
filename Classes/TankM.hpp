84a15399343a6bbbbbce16f49dcd27cbf1b40d45
Amazingkevin <jinlongwu@WuJinlongdeMacBook-Pro.local>
2019-01-01 19:27:18 +0800

    haha

--- Classes/TankM.hpp
+++ Classes/TankM.hpp
@@ -8,6 +8,27 @@
 #ifndef TankM_hpp
 #define TankM_hpp
 
-#include <stdio.h>
+#include "cocos2d.h"
+#include "Global.h"
+#include "PlayerTank.hpp"
+#include "EnemyTank.hpp"
+USING_NS_CC;
+using namespace std;
+class TankM:public Scene
+{
+    
+};
+
+
+
+
+
+
+
+
+
+
+
+
 
 #endif /* TankM_hpp */
